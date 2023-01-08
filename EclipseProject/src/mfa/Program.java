3ba23a9a07750ea12bc74596663ed9cf3c1e7a68
cs <cs@csUbuntu>
2023-01-08 23:30:38 +0300

    Proje olusturma

---
+++ EclipseProject/src/mfa/Program.java
@@ -0,0 +1,15 @@
+package mfa;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Person p1 = new Person("Mike", 178, 42);
+		Person p2 = new Person("Maria", 165, 35);
+		Person p3 = new Person("Steve", 182, 42);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+	}
+
+}
